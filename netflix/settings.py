1c7f9e09bcf6db0dc83134214b8b4e020e547509
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-02 12:03:09 +0300

    ft:migrations -sets up database and makes migrations

--- netflix/settings.py
+++ netflix/settings.py
@@ -78,7 +78,11 @@ WSGI_APPLICATION = 'netflix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'dbnetflix',
+        'USER': 'moringa',
+        'PASSWORD': 'Access',
+        'HOSTS': '',
+        
     }
 }
 
