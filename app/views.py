9d3fe9cc7ef61aa1ac52b20d9e8e79c951f373e0
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-02 08:20:46 +0300

    ft:shows-categories - rebders different novies and shows categories

--- app/views.py
+++ app/views.py
@@ -4,14 +4,59 @@ import  requests
 
 # Create your views here.
 
-def netflix_home(request):
-    poster_url= "https://image.tmdb.org/t/p/w500/"
-    response = requests.get('https://api.themoviedb.org/3/movie/popular?api_key=41ee283e92c80a93f1e33b97fa9b441e')
-    movies_response = response.json()
-    print(movies_response)
-    movies=movies_response['results']
+THE_MOVIE_DB_API_KEY ="41ee283e92c80a93f1e33b97fa9b441e"
 
-    return render (request, 'index.html',{'movies':movies,})
+
+#-----------Movies ------------------------------------------------------
+def netflix_home(request):
+
+    now_playing_movies_request = requests.get("https://api.themoviedb.org/3/movie/now_playing?api_key=" + THE_MOVIE_DB_API_KEY)
+    now_playing_movies_results = now_playing_movies_request.json()
+    now_playing_movies = now_playing_movies_results['results']
+    
+    # popular ones
+    popular = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=" + THE_MOVIE_DB_API_KEY)
+    popular_ones = popular.json()
+    popular_ones_now = popular_ones['results']
+    
+    # top rated ones
+    top_rated_movies = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + THE_MOVIE_DB_API_KEY)
+    top_rated_results = top_rated_movies.json()
+    top_ratings = top_rated_results['results']
+    
+    #Upcoming movies
+    upcoming_movies = requests.get("https://api.themoviedb.org/3/movie/upcoming?api_key=" + THE_MOVIE_DB_API_KEY)
+    upcoming_results = upcoming_movies.json()
+    upcomings = upcoming_results['results']
+    
+    
+    #----TV SHOWS--------------------------------------------------------
+    
+    tv_shows = requests.get("https://api.themoviedb.org/3/tv/popular?api_key=" + THE_MOVIE_DB_API_KEY)
+    tv_results =  tv_shows.json()
+    shows = tv_results['results']
+    
+    
+    airing_today_tv_shows = requests.get("https://api.themoviedb.org/3/tv/airing_today?api_key=" + THE_MOVIE_DB_API_KEY)
+    airing_tv_results =  airing_today_tv_shows.json()
+    airing_shows = airing_tv_results['results']
+    
+    #top rated TV-shows
+    top_rated_tv_shows = requests.get("https://api.themoviedb.org/3/tv/airing_today?api_key=" + THE_MOVIE_DB_API_KEY)
+    top_rated_tv_shows_results = top_rated_tv_shows.json()
+    top_rated = top_rated_tv_shows_results['results']
+    
+    #on the air
+    on_air_results = requests.get("https://api.themoviedb.org/3/tv/on_the_air?api_key=" + THE_MOVIE_DB_API_KEY)
+    on_air_tv_shows_results =  on_air_results.json()
+    on_air = on_air_tv_shows_results['results']
+    
+    
+
+    return render (request, 'index.html',{'movies':now_playing_movies,'popular_movies':popular_ones_now, 'up_comings': upcomings ,'top_movies':top_ratings,  'shows':shows,'airing_shows':airing_shows,'top_rated':top_rated,'on_air':on_air})
+
+
+
 
 # https://api.themoviedb.org/3/movie/550?api_key=41ee283e92c80a93f1e33b97fa9b441e
 
